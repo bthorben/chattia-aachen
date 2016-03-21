5519ab09360692e08e96c98fc7603237ae6fd04f
TStormrider <sir_stormrider@web.de>
2016-03-21 17:23:28 +0100

    Added functionality for daily and weekly dates

--- website/calendar.php
+++ website/calendar.php
@@ -1,4 +1,5 @@
-<?php   
+<?php
+    //Wandelt tägliche und wöchentliche Serientermine um
     function returnCalendarDates($datei, $count) {
       $dateTransEnDe = array(
         'Monday'    => 'Montag',
@@ -46,6 +47,7 @@
           
           
           while(strcmp($line, $end) != 0) {
+          
             $line = chop(fgets($calendar));
             if(preg_match("/^DTEND;/", $line)) { //Gibt Endzeit zurück
               if(preg_match("/VALUE=DATE/", $line)) {
@@ -77,9 +79,58 @@
 
               continue;
             }
+            /*
+            if(preg_match("/^DESCRIPTION:/", $line)) { //Gibt Fließtext der Veranstaltung zurück
+              $text = substr($line, 12);
+
+              continue;
+            }*/
+            
+            if(preg_match("/^RRULE:/", $line)) { //Serientermine
+              $rrule_freq = substr($line, 11);
+              $rrule_freq = strstr($rrule_freq , ';', true);
+              
+              $rrule_interval = substr(strstr($line, 'INTERVAL='), 6);
+              $rrule_interval = strstr($rrule_interval, ';', true);
+              
+              $rrule_count = substr(strstr($line, 'COUNT='), 6);
+              $rrule_count = strstr($rrule_count, ';', true);
+              
+              $rrule_until = substr(strstr($line, 'UNTIL='), 6);
+              $rrule_until = strstr($rrule_until, ';', true);
+              
+              $rrule_recurDay = '';
+              $rrule_recurMonthDay = '';
+              
+              
+              switch($rrule_freq) {
+                case "DAILY":
+                  break;
+                case "WEEKLY":
+                  $rrule_recurDay = substr(strstr($line, 'BYDAY='), 6);
+                  break;
+                case "MONTHLY":
+                  $rrule_recurMonthDay = substr(strstr($line, 'BYMONTHDAY='), 6);
+                  $rrule_recurDay = substr(strstr($line, 'BYDAY='), 6);
+                  echo "Error: Monatliche Termine sind nicht erlaubt";
+                  echo "<br/>";
+                  echo "<br/>";
+                  break;
+                case "YEARLY":
+                  echo "Error: Jährlicher Termine sind nicht erlaubt";
+                  echo "<br/>";
+                  echo "<br/>";
+                  break;
+              }
+              
+                            
+              continue;
+            }
+            
           }
           
           $date = getdate();
+          //$date = getdate(mktime(0,0,0,11,10,2015));
           
           $month = str_pad($date["mon"], 2, '0', STR_PAD_LEFT);
           $day = str_pad($date["mday"], 2, '0', STR_PAD_LEFT);
@@ -87,35 +138,98 @@
           $convertedDate = $date["year"].$month.$day;         
                     
           if($endtime > $convertedDate) {
-
-            if($wholeday == 1) { //Ganztägiges Ereignis
-              $starttimeAnzeige = date('D, j. F Y', mktime(0, 0, 0, substr($starttime, 4, 2), substr($starttime, 6, 2), substr($starttime, 0, 4)));
-              
-              //Die Endzeit bei Ganztägigen ist immer der darauffolgende Tag
-              $endtime = $endtime-1;
-              
-              $endtimeAnzeige = date('D, j. F Y', mktime(0, 0, 0, substr($endtime, 4, 2), substr($endtime, 6, 2), substr($endtime, 0, 4)));
-            } else { //Ereignis mit Start- und Endzeit
-              $starttimeAnzeige = date('D, j. F Y - H:i', mktime(substr($starttime, 9, 2), substr($starttime, 11, 2), substr($starttime, 13, 2), substr($starttime, 4, 2), substr($starttime, 6, 2), substr($starttime, 0, 4)));
-              $endtimeAnzeige = date('D, j. F Y - H:i', mktime(substr($endtime, 9, 2), substr($endtime, 11, 2), substr($endtime, 13, 2), substr($endtime, 4, 2), substr($endtime, 6, 2), substr($endtime, 0, 4)));
-            }
             
+            /*
+            $starttimeAnzeige = convertStartDate($starttime, $wholeday);
+            $endtimeAnzeige = convertEndDate($endtime, $wholeday);
 
             //Umwandlung in Deutsch
             $starttimeAnzeige = strtr($starttimeAnzeige, $dateTransEnDe); 
             $endtimeAnzeige = strtr($endtimeAnzeige, $dateTransEnDe);
+            */
             
-            $termine[] = array('Timestamp' => $starttime,
-                               'Startzeit' => $starttimeAnzeige, 
+            $termine[] = array('Startzeit' => strtotime($starttime),
+                               'Endzeit' => strtotime($endtime),
+                               'Beschreibung' => $description,
+                               'Text' => $text,  
+                               'Ort' => $location,
+                               'Ganztaegig' => $wholeday);           
+          }
+          
+          if(!empty($rrule_freq)) {
+            
+            //Setze Interval default auf 1
+            if(empty($rrule_interval)) {
+              $rrule_interval = 1;
+            }
+            /*
+            
+            echo "Starttime ".$starttimeAnzeige;
+            echo "<br/>";
+            echo "Endtime ".$endtimeAnzeige;
+            echo "<br/>";
+            echo "Description ".$description;
+            echo "<br/>";
+            echo "Freq ".$rrule_freq;
+            echo "<br/>";
+            echo "Interval ".$rrule_interval;
+            echo "<br/>";
+            echo "Count ".$rrule_count;
+            echo "<br/>";
+            echo "Until ".$rrule_until;
+            echo "<br/>";
+            echo "RecurDay ".$rrule_recurDay;
+            echo "<br/>";
+            echo "RecurMonthDay ".$rrule_recurMonthDay;
+            echo "<br/>";
+            echo "<br/>";
+            
+            */
+            
+            if(strcmp($rrule_freq, "DAILY") == 0) { //Tägliches Intervall
+              if(empty($rrule_until)) { //Count gegeben
+                for($i = 1; $i < $rrule_count; $i++) {
+                  $termine[] = array('Startzeit' => (strtotime($starttime)+$rrule_interval*$i*24*3600),
                                'Endzeit' => $endtimeAnzeige,
                                'Beschreibung' => $description, 
                                'Ort' => $location,
-                               'Ganztaegig' => $wholeday);
+                               'Ganztaegig' => $wholeday);    
+                }
+              } else { //Until gegeben
+                for($i = 1; (strtotime($starttime)+$rrule_interval*$i*24*3600) < (strtotime($until)); $i++) {
+                  $termine[] = array('Startzeit' => (strtotime($starttime)+$rrule_interval*$i*24*3600),
+                                'Endzeit' => $endtimeAnzeige,
+                                'Beschreibung' => $description, 
+                                'Ort' => $location,
+                                'Ganztaegig' => $wholeday); 
+                } 
+              }
+            }
+             
+            if(strcmp($rrule_freq, "WEEKLY") == 0) { //Wöchentliches Intervall
+              if(empty($rrule_until)) { //Count gegeben
+                for($i = 1; $i < $rrule_count; $i++) {
+                  $termine[] = array('Startzeit' => (strtotime($starttime)+$rrule_interval*$i*7*24*3600),
+                               'Endzeit' => $endtimeAnzeige,
+                               'Beschreibung' => $description, 
+                               'Ort' => $location,
+                               'Ganztaegig' => $wholeday);    
+                }
+              } else { //Until gegeben
+                for($i = 1; (strtotime($starttime)+$rrule_interval*$i*7*24*3600) < (strtotime($until)); $i++) {
+                  $termine[] = array('Startzeit' => (strtotime($starttime)+$rrule_interval*$i*7*24*3600),
+                                'Endzeit' => $endtimeAnzeige,
+                                'Beschreibung' => $description, 
+                                'Ort' => $location,
+                                'Ganztaegig' => $wholeday); 
+                } 
+              }
+            }
             
-
-            
-            
+            $rrule_freq='';
           }
+          
+          
         }
         else {
           continue;
@@ -132,39 +246,59 @@
       
       //Sortiere Termine aufsteigend nach Startzeit
       foreach ($termine as $nr => $inhalt) {
-        $start[$nr] = strtolower( $inhalt['Timestamp'] );
-        $startA[$nr]  = strtolower( $inhalt['Startzeit'] );
-        $endA[$nr]   = strtolower( $inhalt['Endzeit'] );
+        $start[$nr] = strtolower( $inhalt['Startzeit'] );
+        $end[$nr]   = strtolower( $inhalt['Endzeit'] );
         $desc[$nr] = strtolower( $inhalt['Beschreibung'] );
+        $txt[$nr] = strtolower( $inhalt['Text'] );
         $loc[$nr] = strtolower( $inhalt['Ort'] );
         $ganztae[$nr] = strtolower( $inhalt['Ganztaegig'] );
       }    
       array_multisort($start, SORT_ASC, $termine);
             
       for($i = 0; $i < $count && $i < count($termine); $i++) {
-        //echo $termine[$i]['Beschreibung'];
-        //echo "<br/>";
-        //echo $termine[$i]['Startzeit'];
-        //echo "<br/>";
-        //echo "Ort: ".$termine[$i]['Ort'];
-        //echo "<br/>";
-        //echo "Ende: ".$termine[$i]['Endzeit'];
-        //echo "<br/>";
-        //echo "<br/>";
-        //echo "<br/>";
+        /*
+        echo $termine[$i]['Beschreibung'];
+        echo "<br/>";
+        echo $termine[$i]['Startzeit'];
+        echo "<br/>";
+        echo "Ort: ".$termine[$i]['Ort'];
+        echo "<br/>";
+        echo "Ende: ".$termine[$i]['Endzeit'];
+        echo "<br/>";
+        echo "<br/>";
+        echo "<br/>";
+        */
+        
+                     
+        if($termine[$i]['Ganztaegig']) {
+          $starttimeAnzeige = date('D, j. F Y', $termine[$i]['Startzeit']);
+          $endtimeAnzeige = date('D, j. F Y', $termine[$i]['Endzeit']-1);
+        } else {
+          $starttimeAnzeige = date('D, j. F - H:i', $termine[$i]['Startzeit']);
+          $endtimeAnzeige = date('D, j. F - H:i', $termine[$i]['Endzeit']);
+        }
+        //Umwandlung in Deutsch
+        $starttimeAnzeige = strtr($starttimeAnzeige, $dateTransEnDe); 
+        $endtimeAnzeige = strtr($endtimeAnzeige, $dateTransEnDe);
+        
         
         echo "<div class=\"well well-sm\">";
           echo $termine[$i]['Beschreibung']." ";
-          echo $termine[$i]['Startzeit'];
+          //echo $termine[$i]['Startzeit'];
+          echo $starttimeAnzeige;
           
           //Zeige Enddatum bei mehrtägigem Ereignis an
           if($termine[$i]['Ganztaegig'] == 1 && ($termine[$i]['Startzeit'] != $termine[$i]['Endzeit'])) {
-            echo " - ".$termine[$i]['Endzeit'];    
+            //echo " - ".$termine[$i]['Endzeit']; 
+            echo " - ".$endtimeAnzeige;   
           }
           
           echo "<br/>";
           echo "Ort: ".$termine[$i]['Ort'];
           echo "<br/>";
+          
+          echo $termine[$i]['Text'];
+          
         echo "</div>";
       }
         
